13b9bbf93f7fcd2c189b34b7a9399bf84cad4aba
Nguyen Viet Tan <tan1812003@gmail.com>
2018-11-21 17:01:39 +0700

    Change structure directory

---
+++ Host/WebHost/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHost.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
